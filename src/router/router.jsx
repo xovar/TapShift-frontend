650f94069e12c08884f25b4b2c8cc5b6d4e7fdaa
xovar <xovar9663@gmail.com>
2026-01-24 00:24:29 +0600

    add initial dependencies

---
+++ src/router/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: "",
+      loader: "",
+    },
+  ]);
+
+  export default router;
