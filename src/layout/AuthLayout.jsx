f33ddaa71815f8a9bb22382c82db29f28789b265
xovar <xovar9663@gmail.com>
2026-01-26 15:51:31 +0600

    add responsive in login page

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -5,11 +5,11 @@ import authImg from "../assets/authImage.png"
 export default function AuthLayout() {
   return (
     <div className="">
-      <div className=" h-screen w-full flex flex-col lg:flex-row">
-        <div className="w-[50%] bg-white flex justify-center items-center">
+      <div className=" h-screen w-full flex  flex-row">
+        <div className="w-full  md:w-[50%] bg-white flex justify-center items-center">
           <Outlet/>
         </div>
-        <div className="bg-[#fbfdf0] w-[50%] flex justify-center items-center">
+        <div className="bg-[#fbfdf0] hidden w-0 md:w-[50%] md:flex justify-center items-center">
             <img src={authImg} style={{objectFit: "cover"}} alt="" />
         </div>
       </div>
